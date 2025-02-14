80c9fb252f1d8c1d48c768babcd38d2343d13333
ClarissaAndrade <clarissapa@gmail.com>
2025-02-13 21:38:48 -0300

    listando categorias

--- src/app/components/category/category-list/category-list.component.ts
+++ src/app/components/category/category-list/category-list.component.ts
@@ -11,26 +11,67 @@ import { Category } from '../../../core/types/category';
 @Component({
   selector: 'app-category-list',
   standalone: true,
-  imports: [],
+  imports: [ MatTableModule, MatIconModule, MatButtonModule ],
   templateUrl: './category-list.component.html',
   styleUrl: './category-list.component.css'
 })
-export class CategoryListComponent {
+export class CategoryListComponent implements OnInit {
 
-    categories: Category[] = [];
-    dataSource = [...this.categories];
+  displayedColumns: string[] = ['name', 'parentCategory', 'actions'];
+  categories: Category[] = [];
+  dataSource = [...this.categories];
 
-    constructor(
-      private categoryService: CategoryService, private router: Router, private toastr: ToastrService) {
-    }
+  constructor(
+    private categoryService: CategoryService, private router: Router, private toastr: ToastrService) {
+  }
 
-    ngOnInit(): void {
-      this.categoryService.list()
-        .subscribe(dados => {
-          this.categories = dados;
-          this.dataSource = [...this.categories];
-          console.log(this.categories);
+  ngOnInit(): void {
+    this.categoryService.list()
+      .subscribe(dados => {
+        this.categories = dados;
+        this.dataSource = [...this.categories];
+        console.log(this.categories);
+      });
+  }
+
+  goToTargetPage(): void {
+    this.router.navigate(['/categorias/adicionar']);
+  }
+  
+  delete(id: number) {
+    this.categoryService.delete(id).pipe(
+      tap({
+        // Verifica o sucesso da requisição
+        next: (response) => {
+          this.toastr.success("Registro deletado com sucesso!", "", {closeButton:true});
+        },
+        // Verifica e lida com erros
+        error: (err) => {
+          this.toastr.error("Ocorreu um erro ao deletar o registro!", "", {closeButton:true});
+          console.log(err);
+        },
+      })
+    ).subscribe({
+      // No caso de sucesso, carrega os dados novamente
+      next: () => {
+        this.categoryService.list().subscribe({
+          next: (dados) => {
+            this.categories = dados;
+            this.dataSource = [...this.categories];
+          },
+          error: (err) => {
+            console.error('Erro ao listar os dados:', err);
+          },
         });
-    }
-
+      },
+      // No caso de erro, pode exibir uma mensagem amigável
+      error: (err) => {
+        console.error('Erro na exclusão:', err);
+      },
+    });
+  }
+  
+  editar(id: number) {
+    this.router.navigate(['categorias/editar', id]);
+  }
 }
